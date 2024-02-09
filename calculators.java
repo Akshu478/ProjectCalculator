50ac018fffab19942a43f3684c044bd7aafe343f
Akshay <01fe22bca025@kletech.ac.in>
2024-02-09 20:42:14 +0530

    Update for Area functions

--- calculators.java
+++ calculators.java
@@ -55,6 +55,17 @@ class arithmatic {
     public static void main(String[] args) {
         calculators z = new calculators();
         z.calci();
+        Area m = new Area();
+        System.out.println("Enter base and height for area of triangle:");
+        Scanner sc = new Scanner(System.in);
+        float operand_1 = sc.nextFloat();
+        float operand_2 = sc.nextFloat();
+        double area_triangle = m.Area_of_triangle(operand_1, operand_2);
+        System.out.println("Area of Triangle is :" + area_triangle);
+        System.out.println("Enter Side for area of circle:");
+        float operand_3 = sc.nextFloat();
+        double area_rectangle = m.Area_of_square(operand_1);
+        System.out.println("Area of Rectangle is :" + area_rectangle);
 
     }
 
