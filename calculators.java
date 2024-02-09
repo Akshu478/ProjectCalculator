8920625e93a43bbc021450bdc188d758abe3d57b
Akshay <01fe22bca025@kletech.ac.in>
2024-02-09 22:38:38 +0530

    Code for Mathematical Operations

--- calculators.java
+++ calculators.java
@@ -55,7 +55,22 @@ class arithmatic {
     public static void main(String[] args) {
         calculators z = new calculators();
         z.calci();
+        float opr1 = 30;
+        float opr2 = 15;
+        Math_opr a = new Math_opr();
+        double squre = a.squre(opr1);
+        System.out.println("Squre of Number  is :" + squre);
 
+        double Min = a.Min(opr1, opr2);
+        System.out.println("Minimum Number is :" + Min);
+        double Max = a.Max(opr1, opr2);
+        System.out.println("Maximum  Number is :" + Max);
+        double power = a.power(opr1, opr2);
+        System.out.println("Power of Number is :" + power);
+        double Absolute = a.Absolute(opr1);
+        System.out.println(" Absolute value is " + Absolute);
+        double Round = a.Round(opr1);
+        System.out.println("Round  to the nearest integer is:" + Round);
     }
 
 }
