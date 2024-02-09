1ecede29d7d6e0b668a57126fda0c2b482f3cc26
Akshay <01fe22bca025@kletech.ac.in>
2024-02-09 20:43:28 +0530

    Code of function of Different Areas

---
+++ Area.java
@@ -0,0 +1,10 @@
+
+class Area extends calculators {
+    double Area_of_triangle(float operand_1, float operand_2) {
+        return (0.5 * operand_1 * operand_2);
+    }
+
+    double Area_of_square(float operand_3) {
+        return (operand_3 * operand_3);
+    }
+}
